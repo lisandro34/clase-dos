449dcb7213a596481d089fd875d0af47097be4f5
lisandro34 <reinozolisandro@gmail.com>
2023-09-13 22:56:13 -0300

    se agrega la clase-dos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clase-dos';
+  titulo: string;
+  longitudTitulo: number = 0;
+  headerClass: string = "text-center bg-dark text-light";
+  premio: string = '';
+
+  constructor() {
+    this.titulo = "Bienvenidos a Angular Clase dos";
+
+    setInterval(() => {
+      this.longitudTitulo++;
+      if (this.longitudTitulo > this.titulo.length) {
+        this.longitudTitulo = 1;
+      }
+    }, 400);
+  }
+
+  realizarSorteo() {
+    let azar = Math.random();
+    if (azar > 0.5) {
+      this.premio = "Has gando un Iphone, dejanos tu correo para ir a retirarlo";
+    } else {
+      this.premio = "Lo lamento, segui participando";
+    }
+  }
 }
